031092c67e40d813dcc0217b128d2e01705bf10b
Chessenok <110049758+Chessenok@users.noreply.github.com>
2025-08-09 09:40:00 +0300

    Added Mapper

---
+++ AIParser/MappingTools/IMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIParser.MappingTools
+{
+    internal interface IMapper
+    {
+        public MapModel MapModel(string model, List<string> markets);
+        public MapModel MapModel(List<string> models, string market);
+    }
+}
